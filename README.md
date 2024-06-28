2844f46ff92d1e8fb3caf1f75d2f75b1abf93b40
Kozhemykin Vitaliy <vikozhemyakin@ozon.ru>
2024-06-28 10:35:41 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NachParis
 Module for Rally The troops. 
+
+Для добавления игры надо:
+
+```
+git clone https://github.com/vinalyan/NachParis public/NachParis
+```
+
+Initialize the database:
+
+```
+sqlite3 db < public/NachParis/title.sql
+```
+
---
+++ title.sql
@@ -0,0 +1,3 @@
+insert or replace into titles ( title_id, title_name, bgg ) values ( 'NachParis', '1914: Nach Paris ', 367031 );
+insert or ignore into setups ( title_id, player_count, scenario, options ) values
+	( 'NachParis', 2, '1', '{"optional rules":false}' );
