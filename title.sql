48ce5c3315e78382855c4e4839383aa9d445b00e
Kozhemykin Vitaliy <vikozhemyakin@ozon.ru>
2024-06-28 17:52:46 +0300

    Добавил сценарий

--- title.sql
+++ title.sql
@@ -1,3 +1,3 @@
 insert or replace into titles ( title_id, title_name, bgg ) values ( 'NachParis', '1914: Nach Paris ', 367031 );
 insert or ignore into setups ( title_id, player_count, scenario, options ) values
-	( 'NachParis', 2, '1', '{"optional rules":false}' );
+	( 'NachParis', 2, 'SCENARIO Nº1 - VICTORIOUS RECOVERY AT GUISE', '{"optional rules":false}' );
