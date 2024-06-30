aed46f7037332d181c831b120fae49fe8f16c5b1
Kozhemykin Vitaliy <vikozhemyakin@ozon.ru>
2024-07-01 02:08:33 +0300

    уменьшил карту в два раза. Подогнал под нее параметры гексов

--- play.js
+++ play.js
@@ -23,21 +23,22 @@ const map_h = 9
 function build_hexes() {
 
     //Смещение. По сути начальные кооодиты
-    let xoff = 339
-    let yoff = 882
+    let xoff = 172
+    let yoff = 442
 
-    // TODO сделать небольшое расстоения между гексами, чтобы они друг на друга не заезжали. 
     //Радиус описывающей гекс окружности. Нужен для описания смещений. 
-    let hex_w = 143
+    let hex_w = 71.5
+    //Зазор, чтобы гексы не наезжали друг на друга
+    let gap = 10
 
-
-
+    //TODO сделать зазор между гексами. 
     // Половина длины по горизонтале 
     let hex_h = hex_w
     // Половина длины по вертикати. 
     let hex_v = (hex_w * sqrt(3))/2
 
     function add_hex(x, y) {
+ 
 		return [
 			[ round(x-hex_h/2), round(y-hex_v) ],
 			[ round(x-hex_h),         round(y) ],
@@ -49,13 +50,15 @@ function build_hexes() {
 	}
 
     /*
-        Идем сначала по вертикале. 
-        От 1 до количетсва вертикальных гексов.
+        Берем нулевую вертикальную. 
+        Рисуем вертикальные
+        Потом горизонтальные. 
+        При смещении по горизонтале учитываем, что 
     */
         for(let num_h = 0; num_h < map_h; ++num_h){
             let x = (num_h * (hex_h + hex_h/2)) + xoff         
             for (let num_v = 0; num_v < map_v; ++num_v) {
-                let y = (num_v * hex_v*2) - (hex_v*(num_h%2)) + yoff //тут пытаюсь сделать 
+                let y = (num_v * hex_v*2) - (hex_v*(num_h%2)) + yoff //тут учитываем, что каждая четная колонка ниже нечетной
                 let hex_id = '' + num_h + num_v
                 let hex = ui.hexes[hex_id] = document.createElementNS(svgNS, "polygon") 
                 ui.hex_x[hex_id] = round(x)	
